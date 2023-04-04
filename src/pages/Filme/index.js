9dc7a9bd3076687862d3e753664845b0a325a32e
Gabriel Spiandorello <gabrielspiandorello@gmail.com>
2023-04-04 03:48:06 -0300

    fiz toda a parte de detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,64 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
+import './style.css';
+
 function Filme(){
+
+    const {id} = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "1b9f42640c34fdb63dbdfab9b7f0ce60",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+
+            })
+        }
+
+        loadFilme();
+
+        return () => {
+
+        }
+    }, [])
+
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes do filme...</h1>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>CONFIRA OS DETALHES DO FILME</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar filme</button>
+                <button>
+                    <a href="#">
+                        Assista o trailer aqui
+                    </a>
+                </button>
+            </div>
         </div>
     )
 }
