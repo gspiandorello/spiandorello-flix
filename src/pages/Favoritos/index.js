d2216f20e90945b51aa57cbaa04fc548dd2ee305
Gabriel Spiandorello <gabrielspiandorello@gmail.com>
2023-09-05 22:56:29 -0300

    adding trash and eye icons in Meus Filmes page

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,50 +1,64 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import './style.css';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import trashIcon from "../../assets/trash-icon.svg";
+import eyeIcon from "../../assets/eye-icon.svg";
+import "./style.css";
 
-function Favoritos(){
+function Favoritos() {
+  const [filmes, setFilmes] = useState([]);
 
-    const [filmes, setFilmes] = useState([])
+  useEffect(() => {
+    const minhaLista = localStorage.getItem("@spiandorelloflix");
+    setFilmes(JSON.parse(minhaLista) || []);
+  }, []);
 
-    useEffect(()=>{
-        const minhaLista = localStorage.getItem("@spiandorelloflix");
-        setFilmes(JSON.parse(minhaLista) || [])
-    }, [])
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
 
-    function excluirFilme(id){
-        let filtroFilmes = filmes.filter((item) => {
-            return (item.id !== id)
-        })
+    setFilmes(filtroFilmes);
 
-        setFilmes(filtroFilmes);
+    localStorage.setItem("@spiandorelloflix", JSON.stringify(filtroFilmes));
+    toast.success("Filme removido com sucesso!");
+  }
 
-        localStorage.setItem("@spiandorelloflix", JSON.stringify(filtroFilmes))
-        toast.success("Filme removido com sucesso!");
-    }
+  return (
+    <div className="meus-filmes">
+      <h1>Meus Filmes</h1>
 
-    return(
-        <div className="meus-filmes">
-            <h1>Meus Filmes</h1>
+      {filmes.length === 0 && (
+        <span>Você não possui nenhum filme salvo ainda :( </span>
+      )}
 
-            {filmes.length === 0 && <span>
-                Você não possui nenhum filme salvo ainda :( </span>}
-
-            <ul>
-                {filmes.map((item)=>{
-                    return(
-                        <li key={item.id}>
-                            <span>{item.title}</span>
-                            <div>
-                                <Link to={`/filme/${item.id}`}>Ver detalhes do filme</Link>
-                                <button onClick={()=> excluirFilme(item.id) }>Excluir da lista</button>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+      <ul>
+        {filmes.map((item) => {
+          return (
+            <li key={item.id}>
+              <span>{item.title}</span>
+              <div>
+                <button className="icons">
+                  <Link to={`/filme/${item.id}`} title="Ver detalhes do filme">
+                    <img src={eyeIcon} alt="Eye Icon" className="eye-icon" />
+                  </Link>
+                </button>
+                <button className="icons">
+                  <img
+                    src={trashIcon}
+                    alt="Delete Icon"
+                    className="icon"
+                    onClick={() => excluirFilme(item.id)}
+                    title="Excluir filme da lista"
+                  />
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Favoritos;
